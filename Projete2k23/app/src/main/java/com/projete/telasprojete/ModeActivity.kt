3432644d77b23391de677aef89449a86e9411f44
Vinícius da Silva <90778417+Sadidas@users.noreply.github.com>
2023-09-01 10:14:33 -0300

    Add files via upload

---
+++ Projete2k23/app/src/main/java/com/projete/telasprojete/ModeActivity.kt
@@ -0,0 +1,40 @@
+package com.projete.telasprojete
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import android.widget.TextView
+import androidx.activity.ComponentActivity
+
+class ModeActivity : ComponentActivity() {
+
+    private var grupo_radio: RadioGroup? = null
+    private var radio_mecan: RadioButton? = null
+    private var radio_user: RadioButton? = null
+    private var btn_confirmar: Button? = null
+    private var texto_erro: TextView? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.mecanic_mode)
+
+        grupo_radio = findViewById(R.id.grupo_radio)
+        radio_mecan = findViewById(R.id.radio_mecan)
+        radio_user = findViewById(R.id.radio_user)
+        btn_confirmar = findViewById(R.id.btnConfirmar)
+        texto_erro = findViewById(R.id.tvErro)
+
+        btn_confirmar!!.setOnClickListener {
+            if(radio_mecan!!.isChecked or radio_user!!.isChecked){
+                val intent_princ = Intent(this, MainActivity::class.java)
+                startActivity(intent_princ)
+            }
+            else{
+                texto_erro!!.visibility = TextView.VISIBLE
+            }
+        }
+
+    }
+}
